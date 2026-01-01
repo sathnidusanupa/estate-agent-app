839837fa455b47980e290083246ea72ede6cd20c
sathnidusanupa <sathnidusanupa@gmial.com>
2026-01-02 01:25:46 +0530

    Make Property Page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // This enables the routing
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* We wrap the whole App in BrowserRouter so the "Manager" can control navigation */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
